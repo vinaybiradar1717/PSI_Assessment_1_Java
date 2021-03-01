7e7cc79fa1a941cc018f937774278717abc246a5
Vinay Biradar <46494289+vinaybiradar1717@users.noreply.github.com>
2021-03-01 16:19:59 +0530

    VinayBiradar_vinbirad
    changes my 5th question

--- Question5.java
+++ Question5.java
@@ -1,16 +1,18 @@
 // Q. 5 Write a Java program to find the k largest elements in a given array. Elements in the array can
 // be in any order.
 
+import java.util.*;
 
-class Question2 {
+class Question5 {
     // This is the main method 
     public static void main(String[] args) {
         
         Integer a[] = new Integer[] {1, 4, 17, 7, 25, 3, 100};
         int k = 3;
-	System.out.println("3 largest elements of the said array are:");
-	
-        
+        Arrays.sort(a, Collections.reverseOrder());
+        for(int i=0; i<k; i++){
+            System.out.println(a[i]+" ");
+        }
 
     }
 }
